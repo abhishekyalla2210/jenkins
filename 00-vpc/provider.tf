3dfd52051d02fa1fc9fb5f87c8906e50a7874ca8
abhishkeyalla2210 <abhishekyalla7@gmail.com>
2026-03-26 14:41:18 +0530

    gitandjenkins

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
    backend "s3" {
-    bucket = "jenkins-practices "
+    bucket = "jenkins-practices"
     key    = "terraform"
     region = "us-east-1"
     use_lockfile = false
